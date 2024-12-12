ed2b92632dd1e43d2c0d4f893e78fde69b848981
XCroft Solution <xcroftsolution@gmail.com>
2024-12-12 08:44:03 -0500

    feat: fix minor issue

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -5,19 +5,19 @@ import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
 import { PanelLeft } from "lucide-react"
 
-import { useIsMobile } from "@/registry/new-york/hooks/use-mobile"
-import { cn } from "@/registry/new-york/lib/utils"
-import { Button } from "@/registry/new-york/ui/button"
-import { Input } from "@/registry/new-york/ui/input"
-import { Separator } from "@/registry/new-york/ui/separator"
-import { Sheet, SheetContent } from "@/registry/new-york/ui/sheet"
-import { Skeleton } from "@/registry/new-york/ui/skeleton"
+import { useIsMobile } from "@/components/uihooks/use-mobile"
+import { cn } from "@/components/uilib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
+import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/registry/new-york/ui/tooltip"
+} from "@/components/ui/tooltip"
 
 const SIDEBAR_COOKIE_NAME = "sidebar:state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
